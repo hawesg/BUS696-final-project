868f8d5ada0a4a1482d21892b2beffefb67d93b4
Garrett Hawes <ghawes@uci.edu>
2019-11-08 18:24:27 -0800

    Add stringr package

--- main.R
+++ main.R
@@ -1,11 +1,13 @@
 # Load most commonly used libraries
 
-list.of.packages <- c("tidyverse", "plotly", "here")
+list.of.packages <- c("tidyverse", "plotly", "here", "ggthemes", "stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
 library(tidyverse)
 library("here")
+library("ggthemes")
+library(stringr)
 
 # ---- begin ----
 # Step One: Load Data:
