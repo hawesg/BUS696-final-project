eb8f83931b2e39bb57c8a0f24625cfb1302176d5
Garrett Hawes <ghawes@uci.edu>
2019-11-08 18:26:38 -0800

    Add some data manipulation and move maps to plots

--- code/load.R
+++ code/load.R
@@ -2,84 +2,71 @@
 wine_data <-
   read.csv(here("data", "input", "winemag-data-130k-v2.csv"))
 colors <- read.csv(here("data", "input", "wine-colors.csv"))
+dim(wine_data)
 
 ################### Clean ###################
-# TODO Look up how to drop blank factor for country 
 
-################### JOIN ###################
-wine_data_with_color <- merge(wine_data, colors, by = "variety")
-
-# 3428 records not matching
-nrow(wine_data)
-nrow(wine_data_with_color)
-
-
-
-################### CHOROPLETH MAP ###################
-
-#TODO Look at this for faceted maps, also look at plotly
-
-# Get Lon and Lat to join then replace USA with US since our data set lists it as US also add country column for join
-
-world_map <- map_data("world")
-world_map <-
-  world_map %>% mutate(region = ifelse(region == "USA", "US", region))
-world_map <- world_map %>% mutate (country = region)
-
-# Drop England, US-France and blank for simplicity since there is no entry in world_map
-
-wine_map_DF <-
-  wine_data  %>% mutate (country = as.character(country)) %>%
+wine_data <-
+  wine_data %>% mutate (country = as.character(country), variety = as.character(variety)) %>%
   filter (country != "England" &
             country != "US-France" &
-            country != "")
-head(wine_map_DF)
-# Generate Summary
+            country != "" &
+            variety != "") %>% drop_na(price)
 
-wmap <-
-  wine_data %>%
-  group_by(country) %>%
-  summarize(
-    point_min = min(points, na.rm = TRUE),
-    point_avg = mean(points, na.rm = TRUE),
-    point_max = max(points, na.rm = TRUE),
-    price_min = min(price, na.rm = TRUE),
-    price_avg = mean(price, na.rm = TRUE),
-    price_max = max(price, na.rm = TRUE)
-  ) %>%
-  select(
-    c(
-      "country",
-      "point_min",
-      "point_avg",
-      "point_max",
-      "price_min",
-      "price_avg",
-      "price_max"
+# TODO
+
+################### Feature Generation ##################
+
+# TODO Decide on factors for price groups budget, ... premium, ultra premium? then use cut to devide them into buckets.
+
+# Turn score into categories per https://www.winespectator.com/articles/scoring-scale
+wine_data <-
+  wine_data  %>% mutate (point_cat = cut(
+    wine_data$points,
+    breaks = c(0, 74, 79, 84, 89, 94, 100),
+    labels = c(
+      "Not Recomended",
+      "Mediocre",
+      "Good",
+      "Very good",
+      "Outstanding",
+      "Classic"
     )
-  ) 
+  ))
 
-head(wmap)
 
-# Create data frame with bounderies and values
-wine_country_map <- left_join(wmap, world_map, by = "country")
 
-.to_PNG <- function(pl, filename){
-  png('images/rplot.png')
-  plot(pl)
-  dev.off()
-}
+# Add a column to indicate wheather the wine includes 
+wine_data <-
+  wine_data  %>% mutate (vintage = str_extract(title, "(19\\d{2}|20\\d{2})") )
 
-.map_from_attribute <- function(att, title) {
-  return(ggplot(wine_country_map, aes_string(map_id = "country", fill = att)) +
-    geom_map(map = wine_country_map,  color = "white") +
-    expand_limits(x = wine_country_map$long, y = wine_country_map$lat) +
-    scale_fill_viridis_c(option = "C") + theme_fivethirtyeight() + ggtitle(title))
-}
+                           
+                           
+                           #sub("\\d", title) )
 
-.map_from_attribute("point_min", "Points Min by Country")
-.map_from_attribute("point_avg", "Points Average by Country")
-.map_from_attribute("point_max", "Points Max by Country")
-.map_from_attribute("price_min", "Price Min by Country")
-.map_from_attribute("price_avg", "Price Average by Country")
-.map_from_attribute("price_max", "Price Max by Country")
+
+# Add color
+wine_data_with_color <- merge(wine_data, colors, by = "variety")
+
+nrow(wine_data)
+#[1] 129833
+nrow(wine_data_with_color)
+#[1] 126407
+nrow(wine_data) - nrow(wine_data_with_color)
+# 3426 records missing
+
+options(scipen = 99999)
+#seq(0, 3000, by = 25)
+stats <- wine_data %>% group_by(pr=cut(price, breaks= c(0, 10, 25, 50, 100, 200, 500, 1000, 3500), dig.lab = 5  )) %>% summarize(count = n(), min = min(points), 
+                                                              max = max(points), avg = mean(points), sd = sd(points))
+stats
+
+
+# List of
+setdiff(wine_data$X, wine_data_with_color$X)
+
+
+
+country_levels <- factor(wine_data$country)
+
+levels(country_levels)
