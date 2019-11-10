b4ee84d647bcb712d6797daf7d94f42999605a8b
Garrett Hawes <ghawes@uci.edu>
2019-11-09 18:38:32 -0800

    format for ricky and bart

---
+++ code/ricky.R
@@ -0,0 +1,11 @@
+############ RICKY ################
+
+
+# Break up logical sections like the line at the top and write comments with a single # 
+
+# ie 
+
+
+############ BOX PLOT ################
+
+# I think we should do a box plot like so
