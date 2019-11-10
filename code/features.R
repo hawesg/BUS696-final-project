bc76a20fce6f02a2f6b182d484962d67f1195bf4
Garrett Hawes <ghawes@uci.edu>
2019-11-10 15:02:11 -0800

    Designation

--- code/features.R
+++ code/features.R
@@ -1,7 +1,3 @@
-
-
-
-
 ################### Feature Generation ##################
 
 # TODO Decide on factors for price groups budget, ... premium, ultra premium? then use cut to devide them into buckets.
@@ -21,7 +17,7 @@ wine_data <-
     )
   ))
 
-# TODO maybe change levels to ordered and also drop unused ones
+# TODO maybe change levels to ordered and also drop unused facttors
 
 # Length of title
 wine_data <-
@@ -32,61 +28,73 @@ wine_data <-
 # TODO this isn't quite right 2 options someone goes through the following 100K+ by hand or we call the variable title_has_year
 
 wine_data %>% filter(grepl("(19\\d{2}|20\\d{2})", title)) %>%
-  select(title) %>%
-  unique() %>% View()
-
-
+  select(title) 
+               
 wine_data <-
-  wine_data  %>% mutate (includes_vintage = grepl("(19\\d{2}|20\\d{2})", title))
+  wine_data  %>% mutate (has_year_in_title = grepl("(19\\d{2}|20\\d{2})", title))
 
-# Add a column to indicate wheather the wine includes some variation of reserve
+##### Designation column
 
-# TODO this does not include all of them there is an accent on some letters in some cases I think, in reality probably "serv"
-#      works but someone would have to manually check by scrolling through these
-wine_data %>% filter(grepl("serv", designation, ignore.case = TRUE)) %>%
-  select(designation) %>%
-  unique() %>% View()
+# List of regular expressions to match
 
-wine_data <-
-  wine_data  %>% mutate (is_reserve = grepl("[Rr][ei]serv[ea]", designation))
+patterns <- c(".*([Rr].serv).*", ".*extra dry.*", ".*(dry|trocken).*", ".*brut.*", ".*(estate|grand|casa).*", 
+              ".*single.*", ".*(klassik|classic|tradition|vintage).*", ".*rose.*", ".*barrel s.*", ".*(old v|vieilles).*", 
+              ".*(vineyard|ranch|alpha|branco|broquel).*", ".*(barrel|crianza|cuve).*", ".*unoaked.*", ".*cuve prestige.*", 
+              ".*(blanc|white|bianco).*", ".*(red|tinto|bussia).*", ".*(nouveau|proprietary|signature|selec|premier).*", 
+              ".*lot.*", ".*late.*", ".*(oak|roble).*", ".*(organic|cannubi).*", ".*(port|colheita).*", 
+              ".*(collection|premium|prestige|limited).*", ".*clone.*",  ".*(block|bin).*")
+
+# Values to replace above patterns with
+
+replacements <- c("Reserve", "Extra Dry", "Dry", "Brut", "Estate", "Single Vineyard", "Classic Vintage",  "Rose",  
+                  "Barrel Sample", "Old Vine", "Some Vineyard", "Barrel", "UnOaked", "Finest Champagne", "White", 
+                  "Red", "Signature", "Lot", "Late Harvest", "Oak", "Organic", "Port", "Premium", "Clone", "Block")
+
+# This is so that we can use the replacements object in str_replace_all rather than a single pattern/replacement
+names(replacements) <- patterns
 
 
-# TODO This is not quite right because there is still a few where they have included a year that is not the vintage
-#vintage = str_extract(title, "(19\\d{2}|20\\d{2})") )
+
+wine_designations_no_accents <-
+  wine_data %>% mutate(designation = stringi::stri_trans_general(designation, "Latin-ASCII")) %>% # translate from accented charecters to their non accented counterparts,
+  mutate(designation = tolower(str_squish(str_replace_all(designation, "[^A-Za-z]", " ")))) %>% # get rid of all non letter charecters then trim white space and make lower case
+  mutate(designation = str_replace_all(wine_designations_no_accents$designation, replacements) ) %>% # replace per above
+  mutate(designation = as.factor(designation)) 
+
+summary(wine_designations_no_accents)
+
+ggplot(wine_designations_no_accents, aes(x=designation)) + geom_bar()
+
+
+
+View(wine_designations_no_accents)
+
+dim(wine_designations_no_accents %>% filter(grepl("[¡]", designation)))
+# 1
+dim(wine_designations_no_accents %>% filter(grepl("[\\;]", designation)))
+# 12
+dim(wine_designations_no_accents %>% filter(grepl("[\\-]", designation)))
+# 2037
+
+# Find records that match one but not another
+
+x <- wine_designations_no_accents %>% filter(grepl("r[ei]serv[ae]", designation))
+y <- wine_designations_no_accents %>% filter(grepl("[ei]serv", designation)) 
+setdiff(x,y)
 
 # TODO Maybe a regex for if the title has accents in it (ie seems forign and fancy)
 
-# Get Names Vector
-dput(colnames(wine_data))
-# c(
-#   "X",
-#   "country",
-#   "description",
-#   "designation",
-#   "points",
-#   "price",
-#   "province",
-#   "region_1",
-#   "region_2",
-#   "taster_name",
-#   "taster_twitter_handle",
-#   "title",
-#   "variety",
-#   "winery",
-#   "vintage",
-#   "includes_vintage",
-#   "point_cat",
-#   "title_length",
-#   "is_reserve"
-# )
-
-level(wine_data$taster_name)
-
-wine_data$taster_name
 
 
-# TODO do we drop the reviews that have no name assuming that they should have a reviewer and this is erronious data?
 
-cf <- fct_lump(wine_data$taster_name, n = 5)
-levels(cf)
-? fct_lump
+
+# levels(wine_data$taster_name)
+# 
+# wine_data$taster_name
+# 
+# 
+# # TODO do we drop the reviews that have no name assuming that they should have a reviewer and this is erronious data?
+# 
+# cf <- fct_lump(wine_data$taster_name, n = 5)
+# levels(cf)
+# ? fct_lump
