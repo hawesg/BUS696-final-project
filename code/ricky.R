7014046aa66c8844583331804b76009f6430c028
Ducksforcup <rickylobel@gmail.com>
2019-11-10 16:22:03 -0800

    Twitter Demographics

--- code/ricky.R
+++ code/ricky.R
@@ -1,3 +1,5 @@
+library('tidyverse')
+
 ############ RICKY ################
 
 
@@ -9,3 +11,13 @@
 ############ BOX PLOT ################
 
 # I think we should do a box plot like so
+
+#Made New Twitter Data Set To Provide Context, Included Numbers of Tweets and Number of Followers
+
+table(wine_data$taster_twitter_handle)
+
+TwitterData <- read_excel("data/input/TwitterData.xlsx")
+
+ggplot(TwitterData, mapping = aes(x = number_of_tweets, y = number_of_followers, color = taster_twitter_handle)) + geom_point() + theme_fivethirtyeight() + ggtitle("Twitter Chart")
+
+                                    
