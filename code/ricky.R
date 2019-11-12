b49388b9e983f858efa032028ad797a4cddc35e3
Ducksforcup <rickylobel@gmail.com>
2019-11-12 13:16:51 -0800

    Plots and More Plots

--- code/ricky.R
+++ code/ricky.R
@@ -21,4 +21,16 @@ TwitterData <- read_excel("data/input/TwitterData.xlsx")
 ggplot(TwitterData, mapping = aes(x = number_of_tweets, y = number_of_followers, color = taster_twitter_handle)) + geom_point() + ggtitle("Twitter Chart") + labs(x = "number of tweets", y = "number of followers")
 
 mean(wine_data$price, wine_data$taster_twitter_handle)
+
+
+#Plots
+
+ggplot(wine_data, aes(x = point_cat, y = price)) +
+  geom_bar(color = "purple", stat = "identity") + ggtitle("Point_Cat and Price")
+
+ggplot(wine_data, aes(x = price, y = country_lump, color = point_cat)) +
+  geom_jitter() + ggtitle("Price and Country Colored by Point_Cat")
+
+ggplot(wine_data, aes(taster_review_count, fill = taster_avg_points)) + geom_density(position = "stack", color = "purple") + ggtitle("Density Plot of Twitter Data")
+
                                     
